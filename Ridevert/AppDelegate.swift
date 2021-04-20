aaef3d00157f481653ff2285319a1bf84ca0b6e0
savarams <naidu.srk@gmail.com>
2021-04-20 23:23:09 +0530

    tst1

--- Ridevert/AppDelegate.swift
+++ Ridevert/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        let isLoggedIn = UserDefaults.standard.object(forKey: "isLoggedIn") as! Bool?
+        let isLoggedIn = UserDefaults.standard.object(forKey: "isLoggesdffdIn") as! Bool?
 
         if (isLoggedIn != nil && isLoggedIn == true)
         {
